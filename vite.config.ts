4cc9cbfea41bd7f2d8dab291a061f068444833c8
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 08:50:35 -0800

    Add linaria

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import pluginLinaria from "@linaria/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), pluginLinaria()],
+});
