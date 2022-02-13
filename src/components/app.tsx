86d0f0e18aa661fe1ff07f3e17f83d3a647e829e
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 08:52:06 -0800

    Add basic layout

---
+++ src/components/app.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "../assets/logo.svg";
+import "./app.css";
+
+export function App() {
+  return (
+    <>
+      <header>HEADER</header>
+      <main>
+        <p>App placeholder</p>
+      </main>
+      <footer>FOOTER</footer>
+    </>
+  );
+}
