469aaefb3d1a3c24a65793d03048e8b34d1fc419
Eddie Naff <eddie@eddienaff.dev>
2022-02-14 12:56:52 -0800

    Integrate player controls

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -1,8 +1,12 @@
 import { useActor } from "@xstate/react";
 import React from "react";
 import thief from "../game-assets/images/thief.gif";
+import { usePlayerControls } from "../hooks/use-player-controls";
 import { coordsToPosition } from "../lib/util/coords-to-position";
-import { PlayerActorType } from "../machines/player-machine-types";
+import {
+  DirectionType,
+  PlayerActorType,
+} from "../machines/player-machine-types";
 import { Image, ImageSizeType } from "./image";
 
 interface PlayerProps {
@@ -13,6 +17,16 @@ function Player({ actor }: PlayerProps) {
   const [state, send] = useActor(actor);
   const { coords } = state.context;
   const position = coordsToPosition(coords);
+  usePlayerControls({
+    handleArrowUp: () =>
+      send({ type: "ARROW_BUTTON_CLICKED", direction: DirectionType.Up }),
+    handleArrowDown: () =>
+      send({ type: "ARROW_BUTTON_CLICKED", direction: DirectionType.Down }),
+    handleArrowLeft: () =>
+      send({ type: "ARROW_BUTTON_CLICKED", direction: DirectionType.Left }),
+    handleArrowRight: () =>
+      send({ type: "ARROW_BUTTON_CLICKED", direction: DirectionType.Right }),
+  });
   return (
     <div style={{ position: "absolute", left: position[0], top: position[1] }}>
       <Image src={thief} alt="Thief" size={ImageSizeType.Small} />
