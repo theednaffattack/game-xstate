f2daab3238cce5013d60afc91a11cf13a4e6c25c
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 18:35:46 -0800

    Collect types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+type XCoordType = number;
+type YCoordType = number;
+
+export type CoordsType = [XCoordType, YCoordType];
+
+export enum DirectionType {
+  Left = "Left",
+  Right = "Right",
+  Up = "Up",
+  Down = "Down",
+}
+
+export interface MenuScreenProps {
+  children: React.ReactNode;
+}
