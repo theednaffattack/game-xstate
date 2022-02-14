ac7541148ed3da655fa2eb159169e8eac266b9e5
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 17:48:01 -0800

    Experiment with postcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import pluginLinaria from "@linaria/rollup";
+import postcss from "rollup-plugin-postcss";
+
+const dev = process.env.NODE_ENV !== "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginLinaria()],
+  plugins: [
+    react(),
+    pluginLinaria({ sourceMap: dev }),
+    // postcss({
+    //   extract: "styles.css",
+    // }),
+  ],
 });
