9331ff3652358add23d65396ebdd959a1ef27ecb
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 18:39:12 -0800

    Add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+import { CoordsType } from "../types";
+
+const PLAYER_STARTING_COORDS = [1, 1];
+
+export const GRID_SQUARE_SIZE = 64;
+export const NUMBER_OF_GRID_ROWS = 4;
+export const NUMBER_OF_GRID_COLUMNS = 12;
+export const DOOR_COORDS = [11, 1];
+export const TREASURE_COORDS = [9, 2];
+
+export const PLAYER_STARTING_HEALTH = 3;
+
+export { PLAYER_STARTING_COORDS };
