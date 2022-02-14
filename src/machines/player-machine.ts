880b0859cf0a6d61b1f616a40a00104a0602eb3e
Eddie Naff <eddie@eddienaff.dev>
2022-02-14 13:36:29 -0800

    Add actions and guards to player machine for movement

--- src/machines/player-machine.ts
+++ src/machines/player-machine.ts
@@ -1,6 +1,8 @@
 import { createMachine } from "xstate";
-import { log } from "xstate/lib/actions";
+import { assign, choose, log } from "xstate/lib/actions";
 import { PLAYER_STARTING_COORDS } from "../lib/constants";
+import { getTargetCoords } from "../lib/util/get-target-coords";
+import { isCoordsOnGrid } from "../lib/util/is-coords-on-grid";
 import { CoordsType } from "../types";
 import { PlayerEventType, PlayerStateType } from "./player-machine-types";
 
@@ -9,17 +11,53 @@ export interface PlayerContextType {
 }
 
 export const playerMachine = createMachine<
-  PlayerContext,
+  PlayerContextType,
   PlayerEventType,
   PlayerStateType
->({
-  context: {
-    coords: PLAYER_STARTING_COORDS as CoordsType,
+>(
+  {
+    context: {
+      coords: PLAYER_STARTING_COORDS as CoordsType,
+    },
+    id: "player",
+    initial: "alive",
+    states: {
+      alive: {
+        on: { ARROW_BUTTON_CLICKED: { actions: "onArrowButtonClicked" } },
+      },
+      dead: {},
+    },
   },
-  id: "player",
-  initial: "alive",
-  states: {
-    alive: { on: { ARROW_BUTTON_CLICKED: { actions: log() } } },
-    dead: {},
-  },
-});
+  {
+    actions: {
+      onArrowButtonClicked: choose([
+        {
+          cond: "isSquareAvailable",
+          actions: "move",
+        },
+      ]),
+      move: assign((context: PlayerContextType, event: PlayerEventType) => {
+        const { coords } = context;
+        const { direction } = event;
+        const targetCoords = getTargetCoords({ coords, direction });
+        return { coords: targetCoords };
+      }),
+    },
+    guards: {
+      isSquareAvailable: (
+        context: PlayerContextType,
+        event: PlayerEventType
+      ): boolean => {
+        if (event.type === "ARROW_BUTTON_CLICKED") {
+          const { coords } = context;
+          const { direction } = event;
+          const targetCoords = getTargetCoords({ coords, direction });
+
+          return isCoordsOnGrid(targetCoords);
+        }
+        // ignore all other events
+        return false;
+      },
+    },
+  }
+);
