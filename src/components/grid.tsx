3b2735346c2f6a65e01af4241709efcd5c7fd1a4
Eddie Naff <eddie@eddienaff.dev>
2022-02-13 22:29:02 -0800

    Add basic grid component

---
+++ src/components/grid.tsx
@@ -0,0 +1,80 @@
+import { css } from "@linaria/core";
+import React from "react";
+import {
+  GRID_SQUARE_SIZE,
+  NUMBER_OF_GRID_COLUMNS,
+  NUMBER_OF_GRID_ROWS,
+} from "../lib/constants";
+import { gridCoordsList } from "../lib/util/grid-coords-list";
+
+const gridStyles = css`
+  width: ${NUMBER_OF_GRID_COLUMNS * GRID_SQUARE_SIZE}px;
+  height: ${NUMBER_OF_GRID_ROWS * GRID_SQUARE_SIZE}px;
+  padding: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  border: var(--white-border);
+`;
+
+type GridProps = { children?: React.ReactNode };
+
+export function Layout({ children }: GridProps) {
+  return <section className={gridStyles}>{children}</section>;
+}
+
+interface GridSquareProps {
+  x: number;
+  y: number;
+}
+
+const tabNumbers = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* border: var(--dev-outline); */
+
+  font-variant-numeric: tabular-nums;
+  color: white;
+  & p {
+    font-variant-numeric: tabular-nums;
+    color: white;
+    padding: 0;
+    margin: 0;
+    font-family: monospace;
+  }
+`;
+
+export function GridSquare({ x, y }: GridSquareProps) {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        width: `${GRID_SQUARE_SIZE}px`,
+        height: `${GRID_SQUARE_SIZE}px}`,
+        top: `${x * GRID_SQUARE_SIZE}px`,
+        left: `${y * GRID_SQUARE_SIZE}px`,
+        border: "2px solid white",
+        padding: 0,
+      }}
+    >
+      <div className={tabNumbers}>
+        <p>
+          {x},{y}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export function Grid({ children }: GridProps) {
+  return (
+    <Layout>
+      {gridCoordsList.map(([x, y]) => (
+        <GridSquare key={`${x}-${y}`} x={x} y={y}></GridSquare>
+      ))}
+      {children}
+    </Layout>
+  );
+}
